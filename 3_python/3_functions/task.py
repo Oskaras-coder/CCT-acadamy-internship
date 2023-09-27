97f49ef3ed41cee25676b4b004d20bab4a3b060d
Oskaras Safinas <osafinas@gmail.com>
2023-09-27 16:38:27 +0300

    Added 6th questions answer. 3_functions

--- 3_python/3_functions/task.py
+++ 3_python/3_functions/task.py
@@ -1,27 +1,44 @@
+import itertools
 from typing import Union
 
+
 def test_split_amount() -> bool or None:
-    assert split_amount(11, [5, 2, 1]) == [5, 5, 1]
     assert split_amount(13, [1, 2, 5]) == [5, 5, 2, 1]
     assert split_amount(13, [1, 10]) == [10, 1, 1, 1]
-    assert split_amount(13, [2, 5]) == False
+    assert split_amount(13, [2, 5]) == [5, 2, 2, 2, 2]
     assert split_amount(11.5, [0.5, 1, 10]) == [10, 1, 0.5]
-    print(split_amount(11, [5, 2]))
-    # assert split_amount(11, [5, 2]) == [5, 2, 2, 2]
+    assert split_amount(11, [5, 2]) == [5, 2, 2, 2]
 
 
-def split_amount(amount: Union[int, float], banknotes: list) -> Union[list, bool]:
+def find_combinations_with_sum(banknotes_values, target_amount) -> list:
+    def backtrack(start, target_amount, current_combination) -> list or None:
+        if target_amount == 0:
+            result.append(list(current_combination))
+            return
+        if target_amount < 0 or start == len(banknotes_values):
+            return
+
+        for bill in range(start, len(banknotes_values)):
+            current_combination.append(banknotes_values[bill])
+            backtrack(bill, target_amount - banknotes_values[bill], current_combination)
+            current_combination.pop()
+
+    result = []
+
+    backtrack(0, target_amount, [])
+    return result
+
+
+def split_amount(amount: Union[int, float], banknotes: list[Union[int, float]]) -> Union[list, bool]:
     """Returns a list, consisting of biggest banknotes/coins, which can be used to split the amount or False - if it's
     not possible to split the amount into banknotes"""
+
     banknotes.sort(reverse=True)
-    split_banknotes = []
-    for money_values in banknotes:
-        while amount // money_values != 0:
-            split_banknotes.append(money_values)
-            amount -= money_values
-    if amount != 0:
-        split_banknotes = False
-    return split_banknotes
+    all_possible_combinations = find_combinations_with_sum(banknotes, amount)
+    if sum(all_possible_combinations[0]) == amount:
+        return all_possible_combinations[0]
+    else:
+        return False
 
 
 test_split_amount()
