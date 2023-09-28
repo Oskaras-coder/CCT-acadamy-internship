0d56ead5f34d4213ff837571e0f66d5aaa2e2de9
Oskaras Safinas <osafinas@gmail.com>
2023-09-28 16:38:16 +0300

    Updated task with recursive algorithm

--- 3_python/3_functions/3_task_extended.py
+++ 3_python/3_functions/3_task_extended.py
@@ -1,9 +1,8 @@
 import itertools
-from typing import Union
+from typing import Union, List
 
 
 def test_split_amount() -> bool or None:
-    assert split_amount(1, {10: 100}) == False
     assert split_amount(100, {10: 100, 20: 1, 50: 1}) == [50, 20, 10, 10, 10]
     assert split_amount(13, {1: 10, 2: 1, 5: 5}) == [5, 5, 2, 1]
     assert split_amount(13, {1: 10, 2: 1, 5: 1}) == [5, 2, 1, 1, 1, 1, 1, 1]
@@ -15,33 +14,40 @@ def test_split_amount() -> bool or None:
     assert split_amount(130.5, {0.5: 4, 10: 15, 20: 14, 50: 2}) == [50, 50, 20, 10, 0.5]
     assert split_amount(1.5, {0.1: 3, 0.2: 10}) == [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.1]
     assert split_amount(1.5, {0.1: 3, 0.2: 10, 0.5: 1}) == [0.5, 0.2, 0.2, 0.2, 0.2, 0.2]
-    assert split_amount(0.77, {0.01: 10, 0.05: 3, 0.1: 2, 0.2: 6})
     assert split_amount(0.77, {0.01: 10, 0.05: 3, 0.1: 2, 0.2: 6}) == [0.2, 0.2, 0.2, 0.1, 0.05, 0.01, 0.01]
     assert split_amount(100, {10: 100, 20: 1, 50: 1}) == [50, 20, 10, 10, 10]
+    assert split_amount(1, {10: 100}) == False
 
 
-def find_combinations_with_sum(banknotes_values, target_amount) -> list:
-    """Recursive algorithm"""
-
-    def backtrack(start, target_amount, current_combination) -> list or None:
-        if target_amount == 0:
-            result.append(list(current_combination))
+def recursive_algorithm(amount: Union[int, float], banknotes_values: List[Union[int, float]],
+                        banknotes_amount: List[int]):
+    def backtrack(start: Union[int, float], amount: Union[int, float],
+                  current_combination: List[Union[int, float]]) -> list or None:
+        if round(amount, 2) == 0:
+            is_found = False
+            for value in banknotes_values:
+                if banknotes_amount.count(round(value, 2)) >= current_combination.count(round(value, 2)):
+                    is_found = True
+                else:
+                    is_found = False
+                    break
+            if is_found:
+                return current_combination
             return
-        if target_amount < 0 or start == len(banknotes_values):
+        if amount < 0 or start == len(banknotes_values):
             return
 
         for bill in range(start, len(banknotes_values)):
-            current_combination.append(banknotes_values[bill])
-            backtrack(bill, target_amount - banknotes_values[bill], current_combination)
+            current_combination.append(banknotes_values[round(bill, 2)])
+            result = backtrack(bill, amount - banknotes_values[round(bill, 2)], current_combination)
+            if result:
+                return result
             current_combination.pop()
 
-    result = []
-
-    backtrack(0, target_amount, [])
-    return result
+    return backtrack(0, amount, [])
 
 
-def iterative_algorithm(amount, banknotes_values, banknotes_amount):
+def iterative_algorithm(amount: Union[int, float], banknotes_values: list, banknotes_amount: list) -> list or bool:
     """Iterative algorithm"""
     is_found = False
     for bill in range(0, int(amount / banknotes_values[-1])):
@@ -49,7 +55,7 @@ def iterative_algorithm(amount, banknotes_values, banknotes_amount):
             if round(sum(seq), 2) == amount:
                 is_found = False
                 for value in banknotes_values:
-                    if banknotes_amount.count(round(value,2)) >= seq.count(round(value,2)):
+                    if banknotes_amount.count(round(value, 2)) >= seq.count(round(value, 2)):
                         is_found = True
                     else:
                         is_found = False
@@ -67,9 +73,13 @@ def split_amount(amount: Union[int, float], banknotes: dict) -> Union[list, bool
     list_of_available_banknotes.sort(reverse=True)
     list_of_banknotes = list(banknotes.keys())
     list_of_banknotes.sort(reverse=True)
-    # all_possible_combinations = find_combinations_with_sum(list_of_available_banknotes, amount)
+
     iterative_answer = iterative_algorithm(amount, list_of_banknotes, list_of_available_banknotes)
-    return iterative_answer
+    recursive_answer = recursive_algorithm(amount, list_of_banknotes, list_of_available_banknotes)
+    if iterative_answer == recursive_answer:
+        return recursive_answer
+    elif recursive_answer is None:
+        return False
 
 
 test_split_amount()
