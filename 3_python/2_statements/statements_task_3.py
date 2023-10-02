5668cb92db19dc1a4503b1a7d2f3cf670ae997fc
Oskaras Safinas <osafinas@gmail.com>
2023-10-02 14:03:32 +0300

    Added statements task 3.1 version

---
+++ 3_python/2_statements/statements_task_3.py
@@ -0,0 +1,45 @@
+import re
+from typing import Dict
+
+
+def find_words_with_repeated_letters(input_filename: str) -> list:
+    global list_of_split_words
+    separators = r'[,.!?:;\n ]+'
+    list_of_split_words = re.split(separators, input_filename)
+    enumerated_list_of_split_words = enumerate(list_of_split_words)
+
+    duplicated_words = []
+    for number, value in enumerated_list_of_split_words:
+        is_duplicate = False
+        for word_index in range(number + 1, len(list_of_split_words)):
+            if list_of_split_words[word_index] == value and value not in duplicated_words:
+                letters_list = list(value)
+                for letter in letters_list:
+                    if letters_list.count(letter) > 1:
+                        duplicated_words.append(value)
+                        is_duplicate = True
+                        break
+                if is_duplicate:
+                    break
+    return duplicated_words
+
+
+def find_counts_of_words_with_repeated_letters(input_filename: str) -> dict[str, int]:
+    word_with_repeated_letters = find_words_with_repeated_letters(input_filename)
+    dictionary_of_repeated_words = {word: list_of_split_words.count(word) for word in word_with_repeated_letters}
+    return dictionary_of_repeated_words
+
+
+def find_repeated_letters_counts_in_words(input_filename: str) -> dict[str, int]:
+    repeated_words = find_counts_of_words_with_repeated_letters(input_filename)
+    for word in repeated_words:
+        repeated_words[word] = {letter: word.count(letter) for letter in word if word.count(letter) > 1}
+    return repeated_words
+
+
+with open("input_words.txt") as filename:
+    text = filename.read().lower()
+
+print(find_words_with_repeated_letters(text))
+print(find_counts_of_words_with_repeated_letters(text))
+print(find_repeated_letters_counts_in_words(text))
